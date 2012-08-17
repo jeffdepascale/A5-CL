9fcf6c6090f37ef97cfc864deb1edcfd6cddc43f
jeffdepascale <jdepascale@testpilot111.com>
2012-08-17 14:55:16 -0400

    test setup

--- unitTests/js/tests/Test.js
+++ unitTests/js/tests/Test.js
@@ -2,13 +2,13 @@
 a5.Package('com.testpilot111.tests')
 
 	.Extends('a5.cl.CLUnitTest')
-	.Class('Test', function(cls){
+	.Class('ServiceTest', function(cls){
 		
 		cls.Test = function(){
 			cls.superclass(this);
 		}
 		
 		cls.Override.runTest = function(){
-		
+			
 		}
 })
